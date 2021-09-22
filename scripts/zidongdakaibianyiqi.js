fd13b0d8b89ae5a970c6af3e6b71e54c883abf3c
Xiaowuyoucy <995846732@qq.com>
2021-09-23 03:25:10 +0800

    Form auto backup script's commit

--- scripts/zidongdakaibianyiqi.js
+++ scripts/zidongdakaibianyiqi.js
@@ -3,5 +3,5 @@ var spawn = require('child_process').exec;
 
 // Hexo 3 用户复制这段
 hexo.on('new', function(data){
-  spawn('start  "D:\\Typora\\Typora.exe" ' + data.path);
+  spawn('start  "D:\Program Files\Typora\Typora.exe" ' + data.path);
 });
